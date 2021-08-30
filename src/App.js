c48d4e8d7185f13a353996acbe744870b09dbbe6
MadounMouad <mouadmadoun3@gmail.com>
2021-08-30 23:34:54 +0100

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+    return ( 
+        <div>
+        <h1>heey rf3rger</h1>
+        <h2>heey</h2>
+        </div>
+    );
+}
+
+export default App;
